4d1535ce8e40a96c041f37bc151a42fd65d627c2
Cody Zornes <codyzornes@gmail.com>
2013-12-18 13:20:44 -0800

    First commit

---
+++ driver.cpp
@@ -0,0 +1,17 @@
+/*
+ *	Author: 	Cody Zornes
+ *	Date: 		11/23/2013
+ *	Version: 	1
+ *	Desc:		Initializes the engine
+ */
+ 
+ #include "Game.hpp"
+ 
+int main() {
+	// just like start game
+	
+	Game gamezilla;
+	gamezilla.Start();
+	
+	return 0;
+}

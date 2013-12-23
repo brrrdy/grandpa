5e30cba8a110d5481571014727077d74ccc426e6
Cody Zornes <codyzornes@gmail.com>
2013-12-23 03:53:46 -0800

    Created input handling function to be called in update

--- GameState.hpp
+++ GameState.hpp
@@ -3,7 +3,7 @@
 
 /*
  *	Author: 	Cody Zornes
- *	Date: 		12/16/2013
+ *	Date: 		12/19/2013
  *	Version: 	2
  *	Desc:		The GameState object handles the entry and exit to/from
  *				the different modes of the game
@@ -15,6 +15,8 @@
  
  #include <SFML/Graphics.hpp>
  
+ #include "Player.hpp"
+ 
  class GameState {
  
  public:
@@ -22,7 +24,8 @@
 	virtual ~GameState();
  
 	void loadAssets(std::string filenames);
-	void drawScreen(sf::RenderWindow* window);
+	void drawScreen(sf::RenderWindow & window);
+	void update(sf::Event);
 	
 	virtual void Enter() = 0;
 	virtual void Exit() = 0;
@@ -30,9 +33,14 @@
 	
 protected:
 	std::string _name;
-	std::map<std::string, sf::Texture&> _textures;
-	std::map<std::string, sf::Sprite&> _sprites;
+	std::map<std::string, sf::Texture> _textures;
+	std::vector<sf::Sprite> _sprites;
+	Player _player;
 	
+	sf::Vector2f _playerV;
+	void checkPlayerInput();
+	
+	bool colliding(sf::Sprite & sprite, sf::Sprite & collided, sf::FloatRect & overlap);
 	
  private:
 	// private stuff
